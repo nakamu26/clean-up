72c261e082d724cca0f615f4892fc6d831b82d8e
nakamu26 <n.takanohs@gmail.com>
2023-06-29 21:52:39 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :name, presence: true
+  validates :password, format: { with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
 end
