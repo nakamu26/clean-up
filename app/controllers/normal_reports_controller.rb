f38ffc54c0e1dd4f262f3949cadae1a154efba4a
nakamu26 <n.takanohs@gmail.com>
2023-06-30 20:39:24 +0900

    保存機能を実装

--- app/controllers/normal_reports_controller.rb
+++ app/controllers/normal_reports_controller.rb
@@ -1,9 +1,26 @@
 class NormalReportsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
   end
 
   def new
     @normal_report = NormalReport.new
+  end
+
+  def create
+    @normal_report = NormalReport.new(normal_report_params)
+    if @normal_report.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def normal_report_params
+    params.require(:normal_report).permit(:location_id, :number_of_times_id, :point, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/normal_report.rb
+++ app/models/normal_report.rb
@@ -1,5 +1,13 @@
 class NormalReport < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :location
   belongs_to :number_of_times
+
+  validates :point, presence: true
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :location_id
+    validates :number_of_times_id
+  end
 end
