29f30887e4c30c44aaf18698b638b27eabfa7dc1
nakamu26 <n.takanohs@gmail.com>
2023-07-02 20:30:45 +0900

    new、createアクションを定義、ルーティングを設定

--- app/controllers/special_reports_controller.rb
+++ app/controllers/special_reports_controller.rb
@@ -1,2 +1,20 @@
 class SpecialReportsController < ApplicationController
+  def new
+    @special_report = SpecialReport.new
+  end
+
+  def create
+    @special_report = SpecialReport.new(special_report_params)
+    if @special_report.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def special_report_params
+    params.require(:special_report).permit(:location_id, :category, :point, :comment, :image).merge(user_id: current_user.id)
+  end
 end
