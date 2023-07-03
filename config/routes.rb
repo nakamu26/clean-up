3f4fe92058aec4e93578e010dc0c81fdabd13b64
nakamu26 <n.takanohs@gmail.com>
2023-07-03 21:06:14 +0900

    edit、updateアクションを定義、ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'normal_reports#index'
   resources :normal_reports, only: [:index, :new, :create, :show, :edit, :update]
-  resources :special_reports, only: [:new, :create, :show]
+  resources :special_reports, only: [:new, :create, :show :edit, :update]
 end
