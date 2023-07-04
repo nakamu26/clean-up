e5c04f47f7099e262a7c0a338563e16ff95e8ac6
nakamu26 <n.takanohs@gmail.com>
2023-07-04 19:15:37 +0900

    アソシエーション、バリデーションを設定

--- app/models/illegal_report.rb
+++ app/models/illegal_report.rb
@@ -1,2 +1,12 @@
 class IllegalReport < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :location
+
+  with_options presence: true do
+    validates :point
+    validates :content
+  end
+  validates :location_id, numericality: { other_than: 0, message: "can't be blank" }
 end
