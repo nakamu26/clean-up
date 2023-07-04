d1e0dd890a5b0b9af3955b82aacc859c969e927b
nakamu26 <n.takanohs@gmail.com>
2023-07-04 19:39:19 +0900

    テストコードを実装

--- spec/factories/illegal_reports.rb
+++ spec/factories/illegal_reports.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :illegal_report do
-    
+    location_id { 1 }
+    point { Faker::Lorem.sentence }
+    content { Faker::Lorem.sentence }
+    comment { Faker::Lorem.sentence }
+    association :user
+  
+    after(:build) do |illegal_report|
+      illegal_report.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
