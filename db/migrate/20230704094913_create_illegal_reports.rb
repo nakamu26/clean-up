c1810d1c08f00ed2bb0dc8e4df38b05f6b411371
nakamu26 <n.takanohs@gmail.com>
2023-07-04 18:56:59 +0900

    illegal_reportsテーブルを作成

--- db/migrate/20230704094913_create_illegal_reports.rb
+++ db/migrate/20230704094913_create_illegal_reports.rb
@@ -1,7 +1,11 @@
 class CreateIllegalReports < ActiveRecord::Migration[6.0]
   def change
     create_table :illegal_reports do |t|
-
+      t.integer    :location_id,        null: false
+      t.text       :point,              null: false
+      t.text       :content,            null: false
+      t.text       :comment
+      t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_02_102727) do
+ActiveRecord::Schema.define(version: 2023_07_04_094913) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2023_07_02_102727) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "illegal_reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "location_id", null: false
+    t.text "point", null: false
+    t.text "content", null: false
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_illegal_reports_on_user_id"
   end
 
   create_table "normal_reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -68,6 +79,7 @@ ActiveRecord::Schema.define(version: 2023_07_02_102727) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "illegal_reports", "users"
   add_foreign_key "normal_reports", "users"
   add_foreign_key "special_reports", "users"
 end
