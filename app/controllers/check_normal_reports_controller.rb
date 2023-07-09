3fe6446a119e6b8e2bfad384dfd64d17cb4a945b
nakamu26 <n.takanohs@gmail.com>
2023-07-09 21:29:50 +0900

    保存機能を実装

--- app/controllers/check_normal_reports_controller.rb
+++ app/controllers/check_normal_reports_controller.rb
@@ -1,12 +1,13 @@
 class CheckNormalReportsController < ApplicationController
   def create
-    @check_normal_report = CheckNormalReport.new(illegal_report_params)
+    @normal_report = NormalReport.find(params[:normal_report_id])
+    @check_normal_report = @normal_report.check_normal_reports.new(check_normal_report_params)
     @check_normal_report.save
   end
 
   private
 
-  def illegal_report_params
-    params.require(:normal_report_id).merge(user_id: current_user.id)
+  def check_normal_report_params
+    params.require(:normal_report).permit(:user_id)
   end
 end
